215237d7fb1c319f56d47ea0fcf4b01e3b7069cd
Westwood <annechen112995@gmail.com>
2018-02-19 00:08:07 -0800

    Loading data

---
+++ visualizations.py
@@ -0,0 +1,43 @@
+import numpy as np
+import csv
+
+def loadRatings(fileName):
+    '''
+    Load data from the data.txt file
+
+    Input format: user_id\tmovie_id\trating
+
+    user_id = int
+    movie_id = int
+    rating = int
+    '''
+    ratings = []
+    f = open(fileName, "r")
+
+    for line in f:
+        ratings.append(line.split())
+
+    return np.asarray(ratings, dtype = int)
+
+def loadMovies(fileName):
+    '''
+    Load data from the movies.txt file
+
+    Input format: movie_id\tmovie_title\tUnknown\tAction\tAdventure\tAnimation
+    \tChildrens\tComedy\tCrime\tDocumentary\tDrama\tFantasy\tFilm-Noir\tHorror
+    \tMusical\tMystery\tRomance\tSci-Fi\tThriller\tWar\tWestern
+
+    movie_id = int
+    movie_title = string
+    movie_genres = bool
+    '''
+    movies = []
+    with open(fileName,'r') as f:
+        reader=csv.reader(f,delimiter='\t')
+        for movieData in reader:
+            movies.append(movieData)
+    return modifyMovieData(np.array(movies))
+
+def main():
+    movie_ratings = loadRatings("data.txt")
+    movies = loadMovies("movies.txt")
