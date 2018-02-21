58491581754dbac24d63359ed93426a1743d9528
Chih-Chieh Lee <clee7@caltech.edu>
2018-02-21 00:02:34 -0800

    Updating

---
+++ matrixFactorization.py
@@ -0,0 +1,121 @@
+# Solution set for CS 155 Set 6, 2016/2017
+# Authors: Fabian Boemer, Sid Murching, Suraj Nair
+
+import numpy as np
+from numpy import linalg as LA
+
+
+def grad_U(Ui, Yij, Vj, reg, eta):
+    """
+    Takes as input Ui (the ith row of U), a training point Yij, the column
+    vector Vj (jth column of V^T), reg (the regularization parameter lambda),
+    and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with
+    respect to Ui multiplied by eta.
+    """
+    gradient = (reg * Ui) - (Vj * (Yij - np.dot(Ui, Vj)))
+    ret = eta * gradient
+    return ret
+
+
+def grad_V(Vj, Yij, Ui, reg, eta):
+    """
+    Takes as input the column vector Vj (jth column of V^T), a training point
+    Yij, Ui (the ith row of U), reg (the regularization parameter lambda),
+    and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with
+    respect to Vj multiplied by eta.
+    """
+    gradient = (reg * Vj) - ((Yij - np.dot(Ui, Vj)) * Ui)
+    ret = eta * gradient
+    return ret
+
+
+def get_err(U, V, Y, reg=0.0):
+    """
+    Takes as input a matrix Y of triples (i, j, Y_ij) where i is the index of
+    a user, j is the index of a movie, and Y_ij is user i's rating of movie j
+    and user/movie matrices U and V.
+
+    Returns the mean regularized squared-error of predictions made by
+    estimating Y_{ij} as the dot product of the ith row of U and the jth
+    column of V^T.
+    """
+    n_rows = Y.shape[0]
+    error = 0
+    for row in range(n_rows):
+        i = Y[row, 0] - 1
+        j = Y[row, 1] - 1
+        Y_ij = Y[row, 2]
+        pred = np.dot(U[i, :], V[j, :])
+        dev = (Y_ij - pred)
+        error += dev * dev
+
+    normU = LA.norm(U, 'fro')
+    normU_squared = normU * normU
+    normV = LA.norm(V, 'fro')
+    normV_squared = normV * normV
+
+    error = ((reg / 2.) * (normU_squared + normV_squared)) + (
+        (1 / 2.) * error)
+
+    return error / n_rows
+
+
+def train_model(M, N, K, eta, reg, Y, eps=0.0001, max_epochs=300):
+    """
+    Given a training data matrix Y containing rows (i, j, Y_ij)
+    where Y_ij is user i's rating on movie j, learns an
+    M x K matrix U and N x K matrix V such that rating Y_ij is approximated
+    by (UV^T)_ij.
+
+    Uses a learning rate of <eta> and regularization of <reg>. Stops after
+    <max_epochs> epochs, or once the magnitude of the decrease in regularized
+    MSE between epochs is smaller than a fraction <eps> of the decrease in
+    MSE after the first epoch.
+
+    Returns a tuple (U, V, err) consisting of U, V, and the unregularized MSE
+    of the model.
+    """
+    print("K = %d" % K)
+
+    n_rows = Y.shape[0]
+
+    # Randomly initialize U and V
+    U = np.random.rand(M, K) - 0.5
+    V = np.random.rand(N, K) - 0.5
+    print("U.shape: ", U.shape)
+    print("V.shape: ", V.shape)
+    # Defines variable for keeping track with stopping condition
+    prev_error = get_err(U, V, Y, reg)
+    decrease = None
+
+    for epoch in range(max_epochs):
+        perm = np.random.permutation(n_rows)
+
+        for row in perm:
+            i = Y[row, 0] - 1
+            j = Y[row, 1] - 1
+            Y_ij = Y[row, 2]
+            U[i, :] = U[i, :] - grad_U(U[i, :], Y_ij, V[j, :], reg, eta)
+            V[j, :] = V[j, :] - grad_V(V[j, :], Y_ij, U[i, :], reg, eta)
+
+        new_error = get_err(U, V, Y, reg)
+        print("epoch: %d, error: %f" % (epoch, new_error))
+
+        # Epoch 1
+        if decrease is None:
+            decrease = np.fabs(prev_error - new_error)
+            prev_error = new_error
+            continue
+
+        # Remaining epochs
+        if np.fabs(new_error - prev_error) <= eps * decrease:
+            prev_error = new_error
+            break
+        else:
+            prev_error = new_error
+
+    return (U, V, prev_error)
--- visualizations.py
+++ visualizations.py
@@ -1,10 +1,10 @@
 import csv
-import heapq
-import itertools
 import numpy as np
+import matrixFactorization as mf
 import matplotlib.pyplot as plt
-from operator import itemgetter
-from collections import Counter
+from basicVisualization import basicVisualization
+
+BORDER = "==================================================================="
 
 
 def loadRatings(fileName):
@@ -66,194 +66,70 @@ def loadMovies(fileName):
     return movie_ID, movie_category, movie_genres
 
 
-def getPopularMovies(movie_ratings, movies):
-    '''
-    Return the top ten most popular movies (most rated) and their ratings.
-    '''
-    popularMovies = list(movie_ratings[:, 1])
-    count = Counter(popularMovies)
-    topTen = count.most_common(10)
+def method1_reg(train, test):
+    print(BORDER)
+    print("Method 1 - reg")
+    M = max(max(train[:, 0]), max(test[:, 0])).astype(int)  # users
+    N = max(max(train[:, 1]), max(test[:, 1])).astype(int)  # movies
+    print("Factorizing with ", M, " users, ", N, " movies.")
+    K = 20
 
-    return topTen
+    regs = [10**-4, 10**-3, 10**-2, 10**-1, 1]
+    eta = 0.03  # learning rate
+    E_ins = []
+    E_outs = []
 
+    for reg in regs:
+        print("Training model with reg = %s" % (reg))
+        U, V, E_in = mf.train_model(M, N, K, eta, reg, train)
+        E_ins.append(E_in)
+        E_outs.append(mf.get_err(U, V, test))
 
-def getMovieRatings(movie_ratings):
-    '''
-    Returns a dictionary of {movie_id: np.array([ratings])}
-    '''
-    # dictionary of ratings
-    ratings = {}
-    for _, movie_id, rating in movie_ratings:
-        if movie_id not in ratings:
-            ratings[movie_id] = np.array([rating])
-        else:
-            ratings[movie_id] = np.append(ratings[movie_id], rating)
-    return ratings
-
-
-def getBestMovies(movie_ratings):
-    '''
-    Return the top ten most highly rated movies and their ratings.
-    '''
-    num_highest = 10
-
-    # dictionary of ratings
-    ratings = getMovieRatings(movie_ratings)
-
-    # average ratings
-    avg_ratings = {k: np.mean(ratings[k]) for k in ratings}
-    sorted_avg_ratings = sorted(avg_ratings.items(), key=itemgetter(1),
-                                reverse=True)
-
-    highest_avg_movie = sorted_avg_ratings[:num_highest]
-    highest_avg_movie_ratings = {k: ratings[k] for (k, _) in highest_avg_movie}
-
-    return highest_avg_movie_ratings
+    plt.plot(regs, E_ins, label='$E_{in}$')
+    plt.plot(regs, E_outs, label='$E_{out}$')
+    plt.title('Error vs. reg')
+    plt.xlabel('reg')
+    plt.ylabel('Error')
+    plt.xscale('log')
+    plt.legend()
+    plt.savefig('method1_reg.png')
 
 
-def getThreeGenres(movie_category, genreList):
-    '''
-    Return all movies in three genres (comedy, horror, romance) and their
-    ratings.
-    '''
-    # Index corresponding to genres Comedy, Horror, Romance
-    genreMoviesList = []
-    for i in range(len(genreList)):
-        movieList = []
-        for j in range(len(movie_category)):
-            if (movie_category[j + 1][genreList[i]]) == 1:
-                movieList.append(j + 1)
-        genreMoviesList.append(movieList)
+def method1(train, test):
+    print(BORDER)
+    print("Method 1")
+    M = max(max(train[:, 0]), max(test[:, 0])).astype(int)  # users
+    N = max(max(train[:, 1]), max(test[:, 1])).astype(int)  # movies
+    print("Factorizing with ", M, " users, ", N, " movies.")
+    K = 20
 
-    return genreMoviesList
-
-
-def allRatingsPlot(movie_ratings, directory, title):
-    '''
-    Plot all ratings in MovieLens dataset
-    '''
-    ratings = movie_ratings[:, 2]
-
-    # Plot Histogram
-    hist, _ = np.histogram(ratings, bins=[1, 2, 3, 4, 5, 6])
-    plt.bar(np.arange(1, 6), hist, align='center')
-    plt.title(title)
-    plt.xlabel('Rating')
-    plt.ylabel('Num. Movies')
-    plt.savefig(directory + title + '_Histogram' + '.png', bbox_inches='tight')
-    plt.clf()
-
-
-def popularRatingsPlot(movie_ratings, movies, directory, title):
-    '''
-    Plot all ratings of ten most popular movies
-    '''
-    popularMovies = getPopularMovies(movie_ratings, movies)
-    popularMovieIds = [i[0] for i in popularMovies]
-    topTenRatings = []
-
-    for i in range(len(popularMovieIds)):
-        ratingsList = []
-        for j in range(len(movie_ratings[:, 1])):
-            if movie_ratings[j, 1] == popularMovieIds[i]:
-                ratingsList.append(movie_ratings[j, 2])
-        topTenRatings.append(ratingsList)
-
-    # Plot Histogram
-    plt.style.use('seaborn-deep')
-    plt.hist(
-        [topTenRatings[0], topTenRatings[1], topTenRatings[2], topTenRatings[3],
-        topTenRatings[4], topTenRatings[5], topTenRatings[6], topTenRatings[7],
-        topTenRatings[8], topTenRatings[9]], bins=[1, 2, 3, 4, 5, 6],
-        label = [popularMovieIds[0], popularMovieIds[1], popularMovieIds[2],
-        popularMovieIds[3], popularMovieIds[4], popularMovieIds[5],
-        popularMovieIds[6], popularMovieIds[7], popularMovieIds[8],
-        popularMovieIds[9]])
-    plt.title(title)
-    plt.legend(loc='upper left')
-    plt.xlabel('Rating')
-    plt.ylabel('Num. Ratings')
-    plt.savefig(directory + title + '_Histogram' + '.png', bbox_inches='tight')
-    plt.clf()
-
-
-def bestRatingsPlot(movie_ratings, directory, title):
-    '''
-    Plot all ratings of ten best movies (highest avg. ratings)
-    '''
-
-    # Get {movie_id: np.array([ratings])} of 10 top rated movies
-    bestMovies = getBestMovies(movie_ratings)
-
-    # Get the list of ratings of these movies
-    ratingsOfBestMovies = []
-    for ratings in bestMovies.values():
-        ratingsOfBestMovies += list(ratings)
-
-    # Plot Histogram
-    hist, _ = np.histogram(ratingsOfBestMovies, bins=[1, 2, 3, 4, 5, 6])
-    plt.bar(np.arange(1, 6), hist, align='center')
-    plt.title(title)
-    plt.xlabel('Rating')
-    plt.ylabel('Num. Movies')
-    plt.savefig(directory + title + '_Histogram' + '.png', bbox_inches='tight')
-    plt.clf()
-
-
-def genreRatingsPlot(movie_ratings, movie_ID, movie_category, movie_genres,
-    directory, title):
-    '''
-    Plot all ratings from three genres
-    '''
-    genreList = [5, 11, 14]
-    genreMovies = getThreeGenres(movie_category, genreList)
-    movieRatings = getMovieRatings(movie_ratings)
-    totalRatingsList = []
-
-    for i in range(len(genreMovies)):
-        ratingsList = []
-        for j in range(len(genreMovies[i])):
-            index = int(genreMovies[i][j])
-            ratingsList = ratingsList + list(movieRatings[index])
-        totalRatingsList.append(ratingsList)
-
-    for k in range(len(totalRatingsList)):
-        # Plot Histogram
-        hist, _ = np.histogram(totalRatingsList[k], bins=[1, 2, 3, 4, 5, 6])
-        plt.bar(np.arange(1, 6), hist, align='center')
-        plt.title(movie_genres[genreList[k]] + '_' + title)
-        plt.xlabel('Rating')
-        plt.ylabel('Num. Movies')
-        plt.savefig(
-            directory + movie_genres[genreList[k]] + '_' + title + '_Histogram'
-            + '.png', bbox_inches='tight')
-        plt.clf()
+    reg = 10**-1
+    eta = 0.03  # learning rate
+    U, V, E_in = mf.train_model(M, N, K, eta, reg, train)
+    E_out = mf.get_err(U, V, test)
+    print("E_in = ", E_in)
+    print("E_out = ", E_out)
 
 
 if __name__ == '__main__':
-    # movie_ratings = [[user ID, movie ID, rating]]
-    movie_ratings = loadRatings('data/data.txt')
+    directory = 'visualizations/'
+    dataFile = "data/data.txt"
+    moviesFile = "data/movies.txt"
+    trainingFile = "data/train.txt"
+    testFile = "data/test.txt"
+
+    # all movie ratings as movie_ratings = [[user ID, movie ID, rating]]
+    movie_ratings = loadRatings(dataFile)
+    # movie info as three dictionaries:
     # movie_ID = {movie_id:movie_title}
     # movie_category = {movie_id:categories}
     # movie_genres = {index of genre: genre name}
-    movie_ID, movie_category, movie_genres = loadMovies('data/movies.txt')
+    movie_ID, movie_category, movie_genres = loadMovies(moviesFile)
 
-    directory = 'visualizations/'
-    allRatingsTitle = 'All_Ratings'
-    popularRatingsTitle = 'Top_Ten_Popular_Movie_Ratings'
-    bestRatingsTitle = 'Top_Ten_Best_Movie_Ratings'
-    genreRatingsTitle = 'Movie_Ratings'
+    basicVisualization(movie_ratings, movie_ID, movie_category, movie_genres,
+                       directory)
 
-    # Plotting all ratings in MovieLens dataset
-    allRatingsPlot(movie_ratings, directory, allRatingsTitle)
+    train = loadRatings(trainingFile)
+    test = loadRatings(testFile)
 
-    # Plotting all ratings of ten most popular movies
-    popularRatingsPlot(movie_ratings, movie_ID, directory, popularRatingsTitle)
-
-    # Plotting all ratings of ten best movies (highest avg. ratings)
-    bestRatingsPlot(movie_ratings, directory, bestRatingsTitle)
-
-    # Plotting all ratings from three genres
-    genreRatingsPlot(
-        movie_ratings, movie_ID, movie_category, movie_genres, directory,
-        genreRatingsTitle)
+    method1_reg(train, test)
